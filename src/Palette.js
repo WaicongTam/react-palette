a1a06a2519077b8aded05388f30721ec2338305c
Charles Tam <charles.tan@monash.edu>
2021-05-30 09:37:44 +1000

    Imported and incorporated the colorbox class

--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
 
 class Palette extends Component {
     render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox background={color.color} name={color.name}/>
+        ));
         return (
             <div className="Palette">
-                <h1>PALETTE</h1>
+            
+                <div className="Palette-colors">
+                    {colorBoxes}
+                </div>
             </div>
         );
     }
