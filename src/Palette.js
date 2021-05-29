e0ed06fd9c18aef96ded97f2eea253d8fec75dfd
Charles Tam <charles.tan@monash.edu>
2021-05-29 22:54:21 +1000

    Initialised the Palette structure

--- src/Palette.js
+++ src/Palette.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Palette extends Component {
     render() {
         return (
-            <div>
+            <div className="Palette">
                 <h1>PALETTE</h1>
             </div>
         );
