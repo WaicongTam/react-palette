0b45e394b50752e3ad704d572e2da5b3816a0514
Charles Tam <charles.tan@monash.edu>
2021-05-30 11:16:26 +1000

    Unwrapped the attributes of props

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,6 +3,7 @@ import './ColorBox.css';
 
 class ColorBox extends Component {
     render() {
+        const {name, background} = this.props;
         return (
             <div style={ {background:this.props.background} } className="ColorBox">
                 <span>{this.props.name}</span>
