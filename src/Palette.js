db9130fca461c19a9ef394301ec46cc2819e5326
Charles Tam <charles.tan@monash.edu>
2021-06-19 11:52:29 +1000

    Import the generic style

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import 'rc-slider/assets/index.css';
 import './Palette.css';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css'
+
 
 class Palette extends Component {
     constructor(props) {
